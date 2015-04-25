6b599be694e62d15f3fd1e96d05a73078c61d6d0
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 16:54:06 -0700

    Fix bug in mongoose connection config

--- app/config.js
+++ app/config.js
@@ -2,10 +2,12 @@
 var mongoose = require('mongoose');
 require('./models/user');
 
-var mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/shortlydb';
+var mongoURI = process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost/shortly';
 mongoose.connect(mongoURI)
 var db = mongoose.connection;
 
 db.on('error', function(err){
   console.log(err);
 });
+
+module.exports =db;
