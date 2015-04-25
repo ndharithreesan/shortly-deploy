459da8410458e3babf3ce1977aab672f8fced41a
Hack Reactor Students <accounts@hackreactor.com>
2015-04-25 11:26:10 -0700

    Completed MongoDB refactor. Shortly is now fully functional.
    Next step: refactor for deployment

--- app/models/user.js
+++ app/models/user.js
@@ -5,28 +5,31 @@ var Promise = require('bluebird')
 
 var Schema = mongoose.Schema
 var userSchema = new Schema({
-  name: String,
+  username: {type: String, unique: true},
   password: String,
   createdAt: {type: Date, default: Date.now}
 });
 
 
-userSchema.statics.comparePassword = function(attemptedPassword, callback) {
-  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+userSchema.statics.comparePassword = function(attemptedPassword, user, callback) {
+  bcrypt.compare(attemptedPassword, user['password'], function(err, isMatch) {
     callback(isMatch);
   });
 };
 
-userSchema.statics.hashPassword = function(){
-  // var cipher = Promise.promisify(bcrypt.hash);
-  // return cipher(this.get('password'), null, null).bind(this)
-  //   .then(function(hash) {
-  //     this.set('password', hash);
-  //   });
+userSchema.statics.hashPassword = function(obj, callback){
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(obj['password'], null, null)
+    .then(function(hash) {
+      obj['password'] = hash;
+      callback();
+    });
 };
 
-userSchema.on('init', function(model){
-  model.hashPassword();
+userSchema.pre('save', function(next){
+  User.hashPassword(this, function(){
+    next()
+  });
 });
 
 var User = mongoose.model('User', userSchema);
